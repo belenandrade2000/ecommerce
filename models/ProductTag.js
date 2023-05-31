991828aa464475536bbcd96c60d8b1628cb75b03
Belen Andrade <belenandrade2000@gmail.com>
2023-05-31 00:54:44 -0700

    did models

---
+++ models/ProductTag.js
@@ -0,0 +1,27 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    productTag_id: {
+    type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+  }
+  // refenrece product id and tag id
+
+},
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
