2c8521b3a7698a1b6d75bb99f548c30ab573429f
Belen Andrade <belenandrade2000@gmail.com>
2023-05-31 22:27:07 -0700

    added video link

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -42,10 +42,10 @@ router.get('/:id', async(req, res) => {
 router.post('/', (req, res) => {
   /* req.body should look like this...
     {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
+      "product_name": "Soccer",
+      "price": 200.00,
+      "stock": 3,
+      "tagIds": [1, 2, 3, 4]
     }
   */
   Product.create(req.body)
